0d638a39384e09cce93ec32331a6fa1afedfef11
LuisMarcelo <mar0580@gmail.com>
2025-05-07 23:02:50 -0300

    Renomeado projeto

--- src/main/java/br/com/banco/SistemaBancarioApplication.java
+++ src/main/java/br/com/banco/SistemaBancarioApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class RegistrationLoginDemoApplication {
+public class SistemaBancarioApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(RegistrationLoginDemoApplication.class, args);
+		SpringApplication.run(SistemaBancarioApplication.class, args);
 	}
 
 }
